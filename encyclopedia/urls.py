b5b5b7483d00feb76a557f77b948d8c2460e26a9
Abdurahman215-cmd <alabdalrahman88@gmail.com>
2024-10-11 13:41:31 +0800

    Your commit message

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,20 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.index, name="index"),
-    path("<str:title>", views.entry_page, name="entry_page"),
-    path("search/", views.search, name="search"),
-    path("create/", views.create_page, name="create"),
-    path("<str:title>/edit/", views.edit_page, name="edit"),
-    path("random/", views.random_page, name="random")
-=======
-    path('', views.index, name="index"),
-    path('create/', views.create_page, name='create_page'),
-    path("search/", views.search, name="search"),
-    path('<str:title>/', views.entry_page, name="entry"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("delete/<str:entry_name>/", views.delete, name="delete"),
-    path("random", views.random_page, name="random")
->>>>>>> 7dd0e5fce470c98764fecb4223a73821664844fe
+    path("", views.index, name="index")
 ]
--- entries/HTML.md
+++ entries/HTML.md
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-#HTML
- HTML is a markup language that can be used to define the structure of a web page. HTML elements include
+# HTML
 
- * headings
+HTML is a markup language that can be used to define the structure of a web page. HTML elements include
 
- * paragraphs 
+* headings
+* paragraphs
+* lists
+* links
+* and more!
 
- * lists
-
- * links
-
- * and more!
-
-=======
-HTML is a markup language that can be used to define the structure of a web page. HTML elements include: <br><br>
-* headings <br>
-* paragraphs <br>
-* lists <br>
-* links<br>
-* and more! <br><br>
->>>>>>> 7dd0e5fce470c98764fecb4223a73821664844fe
 The most recent major version of HTML is HTML5.
