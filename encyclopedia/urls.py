a6db82174adfdbfc2f91b5a33758c3d2e25bf7e8
Abdulrahman215-cmd <alabdalrahman88@gmail.com>
2024-10-11 13:46:44 +0800

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -10,13 +10,4 @@ urlpatterns = [
     path("create/", views.create_page, name="create"),
     path("<str:title>/edit/", views.edit_page, name="edit"),
     path("random/", views.random_page, name="random")
-=======
-    path('', views.index, name="index"),
-    path('create/', views.create_page, name='create_page'),
-    path("search/", views.search, name="search"),
-    path('<str:title>/', views.entry_page, name="entry"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("delete/<str:entry_name>/", views.delete, name="delete"),
-    path("random", views.random_page, name="random")
->>>>>>> 7dd0e5fce470c98764fecb4223a73821664844fe
 ]
