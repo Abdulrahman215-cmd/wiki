c3ebd2a9c47e8c0732b7439517ecdd38013b471a
Abdulrahman215-cmd <alabdalrahman88@gmail.com>
2024-10-11 15:42:34 +0800

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", views.index, name="index"),
     path("<str:title>", views.entry_page, name="entry_page"),
     path("search/", views.search, name="search"),
